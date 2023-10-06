9b2746277eb4de97d4aee2c9ca9547e081e56ee3
bipsec <bip.sec22@gmail.com>
2023-10-07 04:20:58 +0600

    fix: updated functionalities

--- bangla_dictionary/script/preprocess.py
+++ bangla_dictionary/script/preprocess.py
@@ -1,39 +1,64 @@
 import pandas as pd
 
+from bangla_dictionary.script.ipa import BanglaIPATranslator
 
-def rename_columns(filename, new_column_names):
-    try:
-        df = pd.read_excel(filename)
+path = "bangla_dictionary/model/ipa_model.pth"
 
-        # dropping null and useless columns
-        df.drop(['Unnamed: 11',
-                 'Unnamed: 12', 'Unnamed: 13', 'Unnamed: 14', 'Unnamed: 15',
-                 'Unnamed: 16', 'Unnamed: 17', 'Unnamed: 18', 'Unnamed: 19',
-                 'Unnamed: 20', 'Unnamed: 21', 'Unnamed: 22', 'Unnamed: 23', 'PoS',
-                 'ভাষা বিষয়ক.1', 'বিষয়-নির্দেশক.1'], axis=1, inplace=True)
-
-        df.columns = new_column_names
-        return df
-
-    except Exception as e:
-        print(f"Error occurred while renaming columns: {e}")
+ipa = BanglaIPATranslator(path)
 
 
 def replace_with_nan(df, values_to_replace):
     for value in values_to_replace:
-        df['word'].replace(value, float('nan'), inplace=True)
+        try:
+            df['word'].replace(value, float('nan'), inplace=True)
+        except KeyError:
+            print(f"Value '{value}' not found in the 'word' column.")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+
+def translate_to_ipa(word):
+    try:
+        ipa_translated = ipa.translate(word)
+        return ipa_translated
+    except Exception as e:
+        print(f"Error translating '{word}' to IPA: {str(e)}")
+        return ""
 
 
 def fill_null_with_previous_word(df):
-    values_to_replace = ['', ' ']
-    replace_with_nan(df, values_to_replace)
+    df['word'].replace(['', ' '], pd.NA, inplace=True)
 
     df['word'].fillna(method='ffill', inplace=True)
+    df['pageNo'].fillna(method='ffill', inplace=True)
+
+    df['word'] = df['word'].str.strip()
 
     df['word'].fillna('', inplace=True)
 
+    value_to_fill_source = "ব্যবহারিক বাংলা অভিধান"
+    df['source'] = value_to_fill_source
+
     return df
 
+
+def fill_ipa(df1, df2, merge_column, suffix1='', suffix2=''):
+    """
+    Merge two DataFrames based on a common column.
+    Check if it needs to rename the column names
+
+    Parameters:
+    - df1: First DataFrame
+    - df2: Second DataFrame
+    - merge_column: The column used for merging
+    - suffix1: Suffix to add to overlapping columns from df1 (default: '')
+    - suffix2: Suffix to add to overlapping columns from df2 (default: '')
+
+    Returns:
+    - Merged DataFrame
+    """
+    merged_df = df1.merge(df2, on=merge_column, how='left', suffixes=(suffix1, suffix2))
+    return merged_df
 
 # Example usage
 # csv_file_path = '../data/demo_data.csv'
