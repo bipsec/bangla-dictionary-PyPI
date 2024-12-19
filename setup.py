242c8ec21d150a281838146678388fdfaeb855b5
bipsec <bip.sec22@gmail.com>
2024-12-19 15:25:23 +0900

    version 9 updated

--- setup.py
+++ setup.py
@@ -70,8 +70,8 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
 
 # Setting up the package
 setup(
-    name="bangla-dictionary",
-    version="0.0.8",
+    name="bangla_dictionary",
+    version="0.0.9",
     author="Biplab Kumar Sarkar, Afrar Jahin, Tanveer Azmal, Asif Shusmit",
     author_email="bip.sec22@gmail.com",
     description="A Complete Bangla Dictionary PyPI Module.",
